3c4df67b464b9b49189b65cf2823539637d524c2
yuri.falbu <yuri.falbu@gmail.com>
2016-09-18 22:31:05 -0300

    adcionando dado de saida em arquivo txt

--- src/modelo/numerico/Main.java
+++ src/modelo/numerico/Main.java
@@ -1,15 +1,20 @@
 package modelo.numerico;
+import jdk.internal.org.objectweb.asm.tree.analysis.Value;
 import org.apache.commons.math.linear.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException {
+        // write your code here
         System.out.println("Multiplicação de matrizes");
-        double[][] matrixData = { {1d,2d,3d}, {2d,5d,3d}};
+        double[][] matrixData = {{1d, 2d, 3d}, {2d, 5d, 3d}};
         RealMatrix m = MatrixUtils.createRealMatrix(matrixData);
 
-        double[][] matrixData2 = { {1d,2d}, {2d,5d}, {1d, 7d}};
+        double[][] matrixData2 = {{1d, 2d}, {2d, 5d}, {1d, 7d}};
         RealMatrix n = new Array2DRowRealMatrix(matrixData2);
 
         RealMatrix p = m.multiply(n);
@@ -20,5 +25,33 @@ public class Main {
 
         // Invertendo a matriz p usando a decomposição LU
 
-        RealMatrix pInverse = new LUDecomposition(p).getL();
+      RealMatrix pInverse = new LUDecompositionImpl(p).getSolver().getInverse();
+        System.out.println(pInverse);
+
+        RealMatrix coeficientes = new Array2DRowRealMatrix(new double[][]{{2,3,-2}, {-1,7,6}, {4,-3,-5}},false);
+
+        DecompositionSolver decompostaQR = new QRDecompositionImpl(coeficientes).getSolver();
+
+        RealVector contantes = new ArrayRealVector(new double[]{1,-2,1}, false);
+
+        RealVector solucao = decompostaQR.solve(contantes);
+        double[][] Q = new QRDecompositionImpl(coeficientes).getQ().getData();
+
+
+        RealMatrix R = new QRDecompositionImpl(coeficientes).getR();
+        System.out.println("----------------------------------------");
+        PrintStream saida = new PrintStream(new FileOutputStream("dados_de_saida.txt"));
+        System.setOut(saida);
+        for (int i = 0; i <= 2; i++){
+            for(int j = 0; j<=2; j++){
+//                System.out.println("Matriz Q "+Q[i][j]);
+                saida.println(Q[i][j]+"\n");
+
+            }
+        }
+        System.out.println("Matriz R"+R);
+        System.out.println("----------------------------------------");
+        System.out.println(solucao);
+
+    }
 }
